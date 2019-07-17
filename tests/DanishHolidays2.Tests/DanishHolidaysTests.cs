43ba6b78ac94a37198d99520257c9e2dd3a81754
Kris Thomsen <mail@kristhomsen.dk>
2019-07-17 21:25:11 +0200

    Initial work on tests

--- tests/DanishHolidays2.Tests/DanishHolidaysTests.cs
+++ tests/DanishHolidays2.Tests/DanishHolidaysTests.cs
@@ -1,20 +1,22 @@
 using System;
 using Xunit;
+using DanishHolidays2;
 
 namespace DanishHolidays2.Tests
 {
     public class DanishHolidaysTests
     {
-        [Fact]
+        [Fact(Skip = "Not yet implemented")]
         public void IsHoliday_Dummy_Test()
         {
-            throw new NotImplementedException();
         }
 
         [Fact]
-        public void GetHolidays_Dummy_Test()
+        public void GetHolidays_ByYear2019_Test()
         {
-            throw new NotImplementedException();
+            var result = DanishHoliday.GetHolidays(2019);
+
+            Assert.Equal(16, result.Count);
         }
     }
 }
